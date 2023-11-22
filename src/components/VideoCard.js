9cb79cc5ddf75037eb71c350ba2bfa86a200a6cd
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-22 17:30:53 +0530

    created video template

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function VideoCard({video}) {
+function VideoCard({ video }) {
   return (
-    <div className='rounded-md bg-[#1E1E1E] '>
+    <Link to={`/VideoDetail/${video?.id?.videoId}`}>
+      <div className='rounded-md bg-[#1E1E1E] '>
         <img src={video?.snippet?.thumbnails?.high?.url} alt={video?.snippet?.title} />
         <div className='p-2 text-white'>
-        <p className='text-base'>{video?.snippet?.title}</p>
-        <p className='text-sm text-gray-500'>{video?.snippet?.channelTitle}</p>
+          <p className='text-base'>{video?.snippet?.title}</p>
+          <p className='text-sm text-gray-500'>{video?.snippet?.channelTitle}</p>
         </div>
-    </div>
+      </div>
+    </Link>
   )
 }
 
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import VideoCard from './VideoCard'
+import { useParams } from 'react-router-dom'
 
 function VideoDetail() {
+  const {videoId} =useParams()
   return (
-    <div>VideoDetail</div>
+    <div className='bg-yellow-500 w-full flex h-full p-2'>
+      <div className='bg-pink-500 w-2/3 h-2/3'>
+        <div className='w-full bg-red-500 h-full'>
+          <video className='w-full h-full'controls>
+            <source src="movie.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className='bg-white'>
+          <p>Video Title</p>
+          <p>Channel name</p>
+        </div>
+      </div>
+      <div className='flex flex-col w-1/3 gap-2 p-2'>
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+
+      </div>
+
+    </div>
   )
 }
 
