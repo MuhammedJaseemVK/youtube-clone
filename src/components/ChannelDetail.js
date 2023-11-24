4e78b3bf6d57b76ce79a2bedecc39c08a11c6d1d
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-24 14:05:57 +0530

    setup channel page

--- src/components/ChannelDetail.js
+++ src/components/ChannelDetail.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from './utils/fetchFromAPI';
+import VideoCard from './VideoCard';
 
 function ChannelDetail() {
+  const { id } = useParams();
+  const [channelData, setChannelData] = useState(null);
+  const [description, setDescription] = useState(false);
+  const [videoData,setVideoData] =useState(null);
+  useEffect(() => {
+    const fetchChannelData = async () => {
+      const response = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+      setChannelData(response.items[0])
+      console.log(response.items[0])
+    }
+    fetchChannelData();
+
+    const fetchVideoData =async()=>{
+      const response= await fetchFromAPI(`search?part=snippet,id&channelId=${id}`);
+      setVideoData(response.items)
+      console.log(response.items)
+    }
+    fetchVideoData();
+
+  }, [id])
   return (
-    <div>ChannelDetail</div>
+    <div className='flex flex-col text-white p-3'>
+      <div className='flex flex-col items-center'>
+        <div className='bg-pink-500 rounded-full w-42 h-42 m-2 flex justify-center items-center overflow-hidden'>
+          <img src={channelData?.snippet?.thumbnails?.default.url} alt="" />
+        </div>
+        <div className='flex flex-col items-center w-1/2'>
+          <p className='text-lg font-bold'>
+            {channelData?.snippet?.title}
+          </p>
+          <p className='text-lg font-bold'>
+            {channelData?.snippet?.customUrl}
+          </p>
+          <div className='flex gap-2'>
+            <p className='text-lg font-bold'>
+              {parseInt(channelData?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            </p>
+            <p className='text-lg font-bold'>
+            {parseInt(channelData?.statistics?.videoCount).toLocaleString()} videos
+            </p>
+          </div>
+          <div className='text-sm text-gray-500 px-50' onClick={() => setDescription(!description)}>
+            {
+              description && channelData?.snippet?.description.length > 50 ? (channelData?.snippet?.description.slice(0, 60) + '...') : channelData?.snippet?.description}
+          </div>
+        </div>
+      </div>
+      <div className='font-bold text-2xl'>
+      {channelData?.snippet?.title}'s videos
+      </div>
+      <div className='grid grid-cols-4 gap-2'>
+        {
+          videoData && videoData.map((video,index,videoData)=>{
+            return <VideoCard key={index} video={video} />
+          })
+        }
+      </div>
+    </div>
   )
 }
 
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Videos from './Videos'
 import { useParams } from 'react-router-dom'
 import { fetchFromAPI } from './utils/fetchFromAPI'
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player/youtube';
+import { Link } from 'react-router-dom';
 
 function VideoDetail() {
   const [video, setVideo] = useState(null);
@@ -32,13 +33,21 @@ function VideoDetail() {
         <div className='bg-pink-500 w-full h-[70vh]'>
           <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} controls height='100%' width='100%' />
         </div>
-        <div className='bg-gray-500 text-white p-2'>
+        <div className='bg-gray-500 text-white p-2 gap-2'>
           <p className='text-4xl font-bold'>{video?.snippet?.title}</p>
           <div className='flex gap-4 text-lg'>
-            <p>{video?.statistics?.viewCount} Views</p>
-            <p>{video?.statistics?.likeCount} Likes</p>
-            <p>{video?.statistics?.commentCount} Comments</p>
+            <p>{parseInt(video?.statistics?.viewCount).toLocaleString()} Views</p>
+            <p>{parseInt(video?.statistics?.likeCount).toLocaleString()} Likes</p>
+            <p>{parseInt(video?.statistics?.commentCount).toLocaleString()} Comments</p>
           </div>
+          <Link to={`/ChannelDetail/${video?.snippet?.channelId}`} className='flex items-center'>
+            <div className='bg-pink-500 rounded-full w-12 h-12 m-2 flex justify-center items-center font-bold'>
+              {video?.snippet?.channelTitle.charAt(0)}
+            </div>
+            <p className='text-lg font-bold'>
+              {video?.snippet?.channelTitle}
+            </p>
+          </Link>
           {
             video?.snippet?.description && (
               <p className='text-base rounded-md bg-gray-700 p-2' onClick={() => setToggleDescription(!toggleDescription)}>{
