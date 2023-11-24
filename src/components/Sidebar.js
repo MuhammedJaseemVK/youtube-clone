fb9ba9ce40d5d99389175a3a37502551043f9981
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-24 15:35:19 +0530

    changed to darktheme colurs.changed categories in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,19 +2,21 @@ import React, { useContext } from 'react'
 import { FaYoutube } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { SidebarContext } from './context/SidebarContext';
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { categories } from './utils/constants';
 
 function Sidebar() {
 
-    const handleClick =(e)=>{
+    const handleClick = (e) => {
         setSelectedCategory(e.target.value);
+        console.log(e.target)
         setShowsidebar(!showSidebar)
     }
 
-    const [showSidebar, setShowsidebar,selectedCategory,setSelectedCategory] = useContext(SidebarContext);
+    const [showSidebar, setShowsidebar, selectedCategory, setSelectedCategory] = useContext(SidebarContext);
     return (
         <div className='fixed top-0 left-0 z-10 w-full h-full flex justify-between'>
-            <div className='h-full w-1/6 bg-slate-700 shadow-xl'>
+            <div className='h-full w-1/6 bg-[#0f0f0f] shadow-xl'>
                 <div className='flex flex-col gap-2 h-full p-4'>
                     <div className='flex gap-3 items-center'>
                         <GiHamburgerMenu className='btn-menu' onClick={() => setShowsidebar(!showSidebar)} />
@@ -23,13 +25,14 @@ function Sidebar() {
                             <p className='font-bold'>YouTube</p>
                         </Link>
                     </div>
-                    <button className='btn' onClick={handleClick} value="Home" >Home</button>
-                    <button className='btn' onClick={handleClick} value="Trending" >Trending</button>
-                    <button className='btn' onClick={handleClick} value="Music" >Music</button>
-                    <button className='btn' onClick={handleClick} value="Movies" >Movies</button>
-                    <button className='btn' onClick={handleClick} value="Animated" >Animated</button>
-                    <button className='btn' onClick={handleClick} value="India">India</button>
-                    <button className='btn' onClick={handleClick} value="Hollywood">Hollywood</button>
+                    {
+                        categories.map((category, index, categories) => (
+                            <button className='btn flex items-center gap-2 text-lg' value={category.name} onClick={handleClick} key={category.name} >
+                                <span>{category.icon}</span>
+                                {category.name}
+                            </button>
+                        ))
+                    }
                 </div>
             </div>
             <div className='bg-black/80 w-5/6 h-full' onClick={() => setShowsidebar(!showSidebar)}>
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,3 +1,33 @@
+import { IoSchoolSharp } from "react-icons/io5";
+import { MdHomeFilled } from "react-icons/md";
+import { FaMusic } from "react-icons/fa";
+import { MdGraphicEq } from "react-icons/md";
+import { MdOndemandVideo } from "react-icons/md";
+import { MdSportsEsports } from "react-icons/md";
+import { MdLiveTv } from "react-icons/md";
+import { IoIosFitness } from "react-icons/io";
+import { MdCheckroom } from "react-icons/md";
+import { MdOutlineFaceRetouchingNatural } from "react-icons/md";
+import { FaTheaterMasks } from "react-icons/fa";
+import { LuCircleDollarSign } from "react-icons/lu";
+import {MdSportsGymnastics} from "react-icons/md";
+
+export const categories = [
+  { name: 'New', icon: <MdHomeFilled />, },
+  { name: 'Music', icon: <FaMusic /> },
+  { name: 'Education', icon: <IoSchoolSharp />, },
+  { name: 'Podcast', icon: <MdGraphicEq />, },
+  { name: 'Movie', icon: <MdOndemandVideo />, },
+  { name: 'Gaming', icon: <MdSportsEsports />, },
+  { name: 'Live', icon: <MdLiveTv />, },
+  { name: 'Sport', icon: <IoIosFitness />, },
+  { name: 'Fashion', icon: <MdCheckroom />, },
+  { name: 'Beauty', icon: <MdOutlineFaceRetouchingNatural />, },
+  { name: 'Comedy', icon: <FaTheaterMasks />, },
+  { name: 'Gym', icon: <MdSportsGymnastics />, },
+  { name: 'Crypto', icon: <LuCircleDollarSign />, },
+];
+
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
